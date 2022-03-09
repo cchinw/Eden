5ab7a343121e25d5c563617816d4aff96645d590
Chinwendu <chinwenduukoha@gmail.com>
2022-03-09 01:21:30 -0500

    finally got my start game to work

--- gameBoard.js
+++ gameBoard.js
@@ -1,14 +1,43 @@
 // Game Tools
 let hasGamestarted = false
 let snakeStart = document.getElementsByClassName(
-  'row7 columns2',
-  'row7 columns3',
-  'row7 columns4'
+  'row7columns2',
+  'row7columns3',
+  'row7columns4'
 )
-let appleStart = document.getElementsByClassName('row7 columns13')
 
-let firstGridBox = document.getElementsByClassName('row0 columns0')
-let snakeDirection = 1
+let snakeBody = [
+  { x: 2, y: 7 },
+  { x: 3, y: 7 },
+  { x: 4, y: 7 }
+]
+
+function snakeIsHere(row, column) {
+  let result = false
+  snakeBody.forEach(function checkPlacement(loc) {
+    if (loc.x === column && loc.y === row) {
+      result = true
+    }
+  })
+  return result
+}
+
+let appleStart = document.getElementsByClassName('row7columns13')
+
+let foodBody = [{ x: 13, y: 7 }]
+
+function foodIsHere(row, column) {
+  let result = false
+  foodBody.forEach(function checkFoofPlacement(loc) {
+    if (loc.x === column && loc.y === row) {
+      result = true
+    }
+  })
+  return result
+}
+
+let snakeDirectionX = 1
+let snakeDirectionY = 0
 let snakeSpeed = 1
 let interval = 0
 let IntervalTime = 0
@@ -21,53 +50,63 @@ let personalBest = 0
 const width = 18
 const height = 15
 
-for (let i = 0; i < height; i++) {
-  for (let j = 0; j < width; j++) {
-    let gridPosition = document.createElement('div')
-    gridPosition.setAttribute('class', `row${i} columns${j}`)
-
-    let grid = document.querySelector('.edenContainer')
-    grid.appendChild(gridPosition)
-  }
-}
-
 //DOM Elements
 let gridBox = document.querySelector('.edenContainer')
 let gameScore = document.getElementsByClassName('.apple')
 let bestScore = document.getElementsByClassName('.bestScore')
-let button = document.querySelector('button')
+let button = document.getElementById('start')
+
+function drawBoard() {
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      let gridPosition = document.createElement('div')
+
+      if (snakeIsHere(i, j)) {
+        gridPosition.setAttribute('class', `row${i}columns${j} snake`)
+      } else if (foodIsHere(i, j)) {
+        gridPosition.setAttribute('class', `row${i}columns${j} food`)
+      } else {
+        gridPosition.setAttribute('class', `row${i}columns${j}`)
+      }
+
+      let grid = document.querySelector('.edenContainer')
+      grid.appendChild(gridPosition)
+    }
+  }
+}
 
 //set arrow key movement event listener
 
 function gameKeys(s) {
-  firstGridBox.className += 'snake'
-  if (s.code === 'ArrowUp') {
-    snakeDirection = -width
+  // let firstGridBox = document.querySelector('.edenContainer').firstElementChild
+
+  // firstGridBox.classList.toggle('snake')
+  const goingUp = snakeDirectionY === -1
+  const goingDown = snakeDirectionY === 1
+  const goingLeft = snakeDirectionX === -1
+  const goingRight = snakeDirectionX === 1
+
+  if (s.code === 'ArrowUp' && !goingDown) {
+    snakeDirectionY = -1
+    snakeDirectionX = 0
     console.log(s.code)
-  } else if (s.code === 'ArrowDown') {
-    snakeDirection = +width
+  } else if (s.code === 'ArrowDown' && !goingUp) {
+    snakeDirectionY = 1
+    snakeDirectionX = 0
     console.log(s.code)
-  } else if (s.code === 'ArrowLeft') {
-    snakeDirection = -1
+  } else if (s.code === 'ArrowLeft' && !goingRight) {
+    snakeDirectionX = -1
+    snakeDirectionY = 0
     console.log(s.code)
-  } else if (s.code === 'ArrowRight') {
-    snakeDirection = 1
+  } else if (s.code === 'ArrowRight' && goingLeft) {
+    snakeDirectionX = 1
+    snakeDirectionY = 0
     console.log(s.code)
   }
 }
 document.addEventListener('keydown', (e) => gameKeys(e))
 
 //Functions for game logic
-
-function appleDefault() {
-  return gridBox[appleStart]
-}
-
-let snakeDefault = (snake) => {
-  snake.forEach((element) => {
-    let snakes = snakeStart.gridBox[element]
-  })
-}
 
 function hideStartButton() {
   let sG = document.getElementById('start')
@@ -78,26 +117,21 @@ function showStartButton() {
   sG.style.display = 'block'
 }
 
-let startGame = (gamePlay) => {
-  gamePlay.forEach((play) => {
-    appleDefault()
-    snakeDefault()
-    hasGamestarted = true
-    hideStartButton()
-    direction = 1
-    appleScore = 0
-    personalBest = 0
-    clearInterval(interval)
-    gameScore.innerText = appleScore
-    bestScore.innerText = personalBest
-    IntervalTime = 1000
-    snakeStart = [128, 129, 130]
-    appleStart = [139]
-    firstGridBox = [0]
-    snakeStart.forEach((box) => gridBox[box].classList.add('snake'))
-    interval = setInterval(movement, IntervalTime)
-  })
+function startGame() {
+  hideStartButton()
+  hasGamestarted = true
+  appleScore = 0
+  personalBest = 0
+  appleScore.innerHTML = appleScore
+  //TODO: Call function to generate random apple
+  drawBoard()
+  clearInterval(interval)
+  interval = setInterval(snakeOutcomes, IntervalTime)
 }
+
 button.addEventListener('click', startGame)
-console.log(snakeDefault)
-console.log(appleDefault)
+
+function snakeOutcomes() {
+  //Snake hitting the wall //Snake hitting its own self
+  //Snake hitting an apple
+}
